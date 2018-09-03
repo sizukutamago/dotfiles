6d47cc900e0a84163af001ab13afe028fc6da566
sizukutamago <tanaka@tam-bourine.co.jp>
2018-09-03 17:38:08 +0900

    add dein

--- init.sh
+++ init.sh
@@ -4,6 +4,4 @@ ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
-mkdir -p ./vim/dein
-
 sh ~/dotfiles/installer.sh ~/dotfiles/vim/dein
