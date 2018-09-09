b1984f3ba79ac1c3351d50d9f26e11b93d60fa68
sizukutamago <tanaka@tam-bourine.co.jp>
2018-09-09 16:25:29 +0900

    tmux

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
 sh ~/dotfiles/installer.sh ~/dotfiles/vim/dein
