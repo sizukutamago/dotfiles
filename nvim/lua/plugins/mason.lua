a9eb25c55e1593769f1c1af420c908bc152051d4
sizukutamago <sizukutamago@gmail.com>
2024-12-17 20:11:35 +0900

    add mason lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,13 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "pylsp",
+    "html",
+    "jsonls",
+    "cssls",
+    "tsserver"
+  }
+})
 require("mason-lspconfig").setup_handlers {
   function(server_name)
     require("lspconfig")[server_name].setup {}
